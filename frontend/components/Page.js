ff799fc0a25589b2dcb467844ec85a216adb3c5a
Elena Novichkova <elenanovichkova@gmail.com>
2018-10-24 16:57:49 -0700

    connect styled nav component

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
 import Header from "./Header.js";
 import Meta from "./Meta.js";
-import styled, { ThemeProvider, injectGlobal } from "styled-components";
 
 const theme = {
   red: "#FF0000",
@@ -16,6 +16,34 @@ const theme = {
 const StyledPage = styled.div`
   background: white;
   color: ${props => props.theme.black};
+`;
+
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+  button {  font-family: 'radnika_next'; }
 `;
 
 const Inner = styled.div`
